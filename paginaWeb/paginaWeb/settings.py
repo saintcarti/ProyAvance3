9270c1a47c2d867a4d4ed65479306e5102a91687
Juakrok <joa.osses@duocuc.cl>
2024-06-27 18:15:08 -0400

    Cmabios index planes

--- paginaWeb/paginaWeb/settings.py
+++ paginaWeb/paginaWeb/settings.py
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
     'cuentas',
     'inventario',
     'crispy_forms',
-    'crispy_bootstrap5',
+    'crispy_bootstrap4',
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK='bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK='bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
