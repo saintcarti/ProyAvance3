9a5535e5b21de47e994810ff69399a82eedbe472
saintcarti <xa.liberona@duocuc.cl>
2024-06-26 20:52:18 -0400

    cambios 2

--- paginaWeb/inventario/urls.py
+++ paginaWeb/inventario/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import inventario_view, API,listado_camaras
+from . import views
 
 urlpatterns = [
-    path('mercado/', inventario_view, name='inventario'),
-    path('API/', API, name='API'),
-    path('listado_camaras/',listado_camaras, name='listado_camaras'),
+    path('mercado/', views.inventario_view, name='inventario'),
+    path('API/', views.API, name='API'),
+    path('inventario_stock/',views.listado_camaras, name='listado_camaras'),
+    path('inventario_stock/detalle_camara/<int:id>/',views.detalle_camara, name= 'detalle_camara'),
+    path('inventario_stock/crear_camara/',views.crear_camara, name= 'crear_camara'),
+    path('inventario_stock/editar_camara/<int:id>/',views.editar_camara, name= 'editar_camara'),
+
 ]
--- paginaWeb/inventario/views.py
+++ paginaWeb/inventario/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Camara
 from .forms import CamaraForm
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def inventario_view(request):
@@ -15,4 +16,29 @@ def listado_camaras(request):
     context = {
         'camaras':camaras
     }
-    return render(request,'Inventario/listado_camaras.html',context)
+    return render(request,'crud/listado_camaras.html',context)
+
+def detalle_camara(request,id):
+    camara = get_object_or_404(Camara, id)
+
+    context= {
+        'camara':camara
+    }
+
+    return render(request,'crud/detalle_camara.html',context)
+
+def editar_camara(request,id):
+    camaras = Camara.objects.all()
+    context= {
+
+        'camaras':camaras
+    }
+
+    return render(request,'crud/editar_camara.html',context)
+
+def crear_camara(request):
+    form = CamaraForm()
+    context = {
+        'form':form
+    }
+    return render(request,'crud/crear.html',context)
